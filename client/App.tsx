6e2c5b6143f0c228495febc3622b9b3282e614f2
Matt Breeden <breeden@gnuidea.net>
2018-06-15 17:33:40 -0400

    sidebar at app level if admin

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import { Sidebar, Menu } from 'semantic-ui-react';
+import { Sidebar, Menu, Segment } from 'semantic-ui-react';
 import ApplicationCtrl from './ApplicationCtrl';
-import { RouteComponentProps, withRouter} from "react-router";
+import { RouteComponentProps, withRouter } from "react-router";
+import { RoleName } from '../shared/models/UserModel'
+import DecisionIcon from '-!svg-react-loader?name=Icon!./img/decisions.svg';
+import ICommonComponentState from '../shared/base-sapien/client/ICommonComponentState'
+import ApplicationViewModel from '../shared/models/ApplicationViewModel'
 
-
-class App extends React.Component<RouteComponentProps<any>, any>
+class App extends React.Component<RouteComponentProps<any>, ApplicationViewModel & ICommonComponentState>
 {
 
     //----------------------------------------------------------------------
@@ -25,31 +28,56 @@ class App extends React.Component<RouteComponentProps<any>, any>
         super(props);
 
         this.controller = new ApplicationCtrl(this);
+        console.log("DATA STORE: ",this.controller.dataStore)
         this.state = this.controller.dataStore;
     }
 
     render() {
-        const ComponentFromState = this.controller.dataStore.ComponentsFistma.currentState
-        return <>
-            <Sidebar
-                as={Menu}
-                animation='uncover'
-                width='thin'
-                visible={true}
-                vertical
-            >
-                <Menu.Item name='home'>
-                    Manage Users
-                    </Menu.Item>
-                <Menu.Item name='home'>
-                    Manage Games
-                    </Menu.Item>
-                <Menu.Item name='home'>
-                    <Link to="/game">Edit Game Content</Link>
-                </Menu.Item>
-            </Sidebar>
-            <ComponentFromState />
+        if (this.state && this.state.ComponentFistma) {
+            const ComponentFromState: any = this.state.ComponentFistma.currentState
+            return <>
+                <Sidebar.Pushable
+                    style={{ height: '100vh' }}
+                >
+                    {this.state && this.controller.CurrentUser && this.controller.CurrentUser.Role == RoleName.ADMIN &&
+                        <Sidebar
+                            as={Menu}
+                            animation='uncover'
+                            width='thin'
+                            visible={true}
+                            vertical
+                        >
+                            {ComponentFromState && ComponentFromState.WrappedComponent.name}
+                            <Menu.Item name='home'>
+                                Manage Users
+                            <DecisionIcon height={20} />
+                            </Menu.Item>
+                            <Menu.Item name='home'>
+                                <a onClick={e => {
+                                    e.preventDefault();
+                                    this.controller.navigateOnClick("/admin/gamelist")
+                                }}>Manage Games</a>
+                            </Menu.Item>
+                            <Menu.Item name='home'>
+                                <a onClick={e => {
+                                    e.preventDefault();
+                                    this.controller.navigateOnClick("/game")
+                                }}>Edit Game Content</a>
+                            </Menu.Item>
+                        </Sidebar>}
+                    <Sidebar.Pusher
+                        className="source-stream"
+                    >
+                        <ComponentFromState />
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+
             </>
+        } else if(!this.state) {
+            return <h2>Loading</h2>
+        } else {
+            return <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        }
     }
 }
 //            <Login/>
