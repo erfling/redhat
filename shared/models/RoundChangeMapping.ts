98e7e28d4ca93cee37a4dcdc6ec8758258d1f9db
David Hamiter <david@ionosoft.com>
2018-07-13 11:59:13 -0400

    comment

--- shared/models/RoundChangeMapping.ts
+++ shared/models/RoundChangeMapping.ts
@@ -3,5 +3,5 @@ import { JobName } from './UserModel';
 export default interface RoundChangeMapping{
     ParentRound: string;
     ChildRound: string;
-    UserJobs: [{[index:string]: JobName}];
+    UserJobs: [{[index:string]: JobName}]; // array of objects, where key is user's _id as string & value is one of JobName enum values
 }
