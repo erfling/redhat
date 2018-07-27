7a732c438e24ae794737dbbb6329f60648abc762
David Hamiter <david@ionosoft.com>
2018-07-27 18:21:23 -0400

    Adding resolveScore. WIP

--- shared/models/ResponseModel.ts
+++ shared/models/ResponseModel.ts
@@ -40,4 +40,43 @@ export default class ResponseModel extends BaseModel
 
     @dbProp(String)
     public ComparisonLabel: ComparisonLabel;
+
+
+    public resolveScore(): number {
+        const ans: ValueObj[] = this.Answer as ValueObj[];
+        var maxScore: number = 0;
+        var score: number = 0;
+
+        ans.forEach((val) => {
+            if (val.maxPoints != null && !isNaN(parseFloat(val.data))) {
+                maxScore += val.maxPoints;
+            } else console.log("Where the hell is 'maxPoints' for", val);
+
+            if (val.data != null) {
+                if (!isNaN(parseFloat(val.data))) {
+                    // normalizedScore as percent in min/max range
+                    let min = val["min"] ? val["min"] : val.minPoints;
+                    let max = val["max"] ? val["max"] : val.maxPoints;
+                    var ratio = (parseFloat(val.data) - min) / (max - min);
+                    score += val.maxPoints * ratio;
+                } else if (val.data === "true" || val.data === true || val.data === "false" || val.data === false) {
+                    if (val.idealValue != null) {
+                        score += (val.data == val.idealValue) ? val.maxPoints : val.minPoints;
+                    }
+                } else {
+                    console.log("Not sure what type scoring for 'data' that doesn't resolve to number or bool!");
+                }
+            } else if (val.label != null) {
+                // I assume we do string-matching against possible answers...
+
+            } else {
+                console.log("Neither 'data' nor 'label' exists, so can't score!");
+            }
+        });
+
+        if (score > maxScore) console.log("Something fugged up! Score", score, "is higher than response's maxScore", maxScore);
+
+        return score;
+    }
+
 }
