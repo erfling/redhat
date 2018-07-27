a6a1a12a2ff1cb7605dc306371cf4a097357e5b9
Matt Breeden <breeden@gnuidea.net>
2018-07-27 13:16:10 -0400

    sub repo sync

--- client/game/MessageList.tsx
+++ client/game/MessageList.tsx
@@ -46,8 +46,8 @@ export default class MessageList extends React.Component<MessageProps, { Message
                         onClick={e => this.props.SelectFunc(m)}
                         className={!m.IsRead ? "bold" : ""}
                     >
-                        {m.Title && <List.Header>{m.Title} | {m._id}</List.Header>}
-                        {!m.Title && <List.Header>{this._stripHtml(m.Content).slice(0, 40) + "..."} | {m._id}</List.Header>}
+                        {m.Title && <List.Header>{m.Title}</List.Header>}
+                        {!m.Title && <List.Header>{this._stripHtml(m.Content).slice(0, 40) + "..."}</List.Header>}
                         <List.Description>{this._stripHtml(m.Content).slice( m.Title ? 0 : 41, m.Title ? 40 : 80 ) + "..."}</List.Description>
                     </List.Content>
                 
