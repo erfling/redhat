363b607bdc42b894ec8ad38b5be19077a644883c
Matt Breeden <breeden@gnuidea.net>
2018-09-07 09:34:07 -0400

    come on

--- client/game/SalesRound/DealRenewal.tsx
+++ client/game/SalesRound/DealRenewal.tsx
@@ -196,6 +196,8 @@ export default class DealRenewal extends BaseComponent<any, IRoundDataStore & {F
                                             r.ValidationMessage = null; 
                                         }
                                         r.SubRoundId = thisSubRound._id
+                                        q.SubRoundId = thisSubRound._id;
+
                                         this.controller.updateResponse(q, r)
                                     }}
                                     IsEditable={this.state.ApplicationState.CurrentUser.Role == RoleName.ADMIN}
@@ -212,7 +214,6 @@ export default class DealRenewal extends BaseComponent<any, IRoundDataStore & {F
                                         q.Response.SkipScoring = true;
                                         q.Response.MaxScore = 1;
                                         q.Response.Score = 100 / q.Response.Answer[0].data;
-
                                         console.log("COMPONENT BEFORE SUBMIT", q.Response);
                                         this.controller.SavePlayerRating(q.Response, q, thisSubRound)
                                     }}
