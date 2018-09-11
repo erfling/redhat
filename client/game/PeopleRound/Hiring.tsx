81b3b70b0afc18c621eb51bce9cd908270f4a85a
Matt Breeden <breeden@gnuidea.net>
2018-09-11 12:15:03 -0400

    hide content while rating users

--- client/game/PeopleRound/Hiring.tsx
+++ client/game/PeopleRound/Hiring.tsx
@@ -200,7 +200,7 @@ export default class Hiring extends BaseComponent<any, IRoundDataStore>
                 </div>
                 }
 
-                {thisSubRound && !this.state.ApplicationState.ShowMessageList && !this.state.ApplicationState.ShowQuestions && this.state.ApplicationState.SelectedMessage && !this.state.ApplicationState.ShowFeedback && !this.state.ApplicationState.ShowIndividualFeedback &&
+                {thisSubRound && !this.state.ApplicationState.ShowMessageList && !this.state.ApplicationState.ShowQuestions && this.state.ApplicationState.SelectedMessage && !this.state.ApplicationState.ShowFeedback && !this.state.ApplicationState.ShowRateUsers && !this.state.ApplicationState.ShowIndividualFeedback &&
                     <EditableContentBlock
                         IsEditable={this.state.ApplicationState.CurrentUser.Role == RoleName.ADMIN}
                         SubRoundId={thisSubRound._id}
--- client/game/SalesRound/DealRenewal.tsx
+++ client/game/SalesRound/DealRenewal.tsx
@@ -255,7 +255,7 @@ export default class DealRenewal extends BaseComponent<any, IRoundDataStore & { 
                 }
 
 
-                {thisSubRound && !this.state.ApplicationState.ShowMessageList && !this.state.ApplicationState.ShowQuestions && this.state.ApplicationState.SelectedMessage && !this.state.ApplicationState.ShowFeedback && !this.state.ApplicationState.ShowIndividualFeedback &&
+                {thisSubRound && !this.state.ApplicationState.ShowMessageList && !this.state.ApplicationState.ShowQuestions && this.state.ApplicationState.SelectedMessage && !this.state.ApplicationState.ShowFeedback && !this.state.ApplicationState.ShowRateUsers && !this.state.ApplicationState.ShowIndividualFeedback &&
                     <EditableContentBlock
                         IsEditable={this.state.ApplicationState.CurrentUser.Role == RoleName.ADMIN}
                         SubRoundId={thisSubRound._id}
