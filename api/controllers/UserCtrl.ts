51524856045aa6d96c47de089f5fbf40acb5bb82
Matt Breeden <breeden@gnuidea.net>
2018-09-25 11:45:51 -0400

    better error handling

--- api/controllers/UserCtrl.ts
+++ api/controllers/UserCtrl.ts
@@ -114,7 +114,8 @@ class UserRouter
         try{
             if(!userToSave.Email || !userToSave.Email.length || !userToSave._id) {
                 var savedUser = await monUserModel.create(userToSave).then(r => r.toJSON() as UserModel);
-
+                if (!savedUser) throw new Error("no user");
+                
                 //invite new user to create admin password
                 if(savedUser.Role == RoleName.ADMIN){
                     let token = Auth.ISSUE_NEW_USER_JWT(savedUser);
@@ -126,6 +127,7 @@ class UserRouter
 
             } else {
                 var savedUser = await monUserModel.findByIdAndUpdate(userToSave._id, userToSave, {new: true}).then(r => r.toObject() as UserModel);
+                if (!savedUser) throw new Error("no user on update");
 
                 //invite user who's role has changed to ADMIN to create admin password
                 if( savedUser.Role == RoleName.ADMIN && userToSave.RoleChanged ){
