394d35cc8538a1063535dd4d9a0d25299273ca1c
Matt Breeden <breeden@gnuidea.net>
2018-09-25 12:05:27 -0400

    logging

--- client/admin/UserManagementCtrl.ts
+++ client/admin/UserManagementCtrl.ts
@@ -119,7 +119,7 @@ export default class UserManagementCtrl extends BaseClientCtrl<IControllerDataSt
             this.dataStore.Admin.AdminUsers = this.dataStore.Admin.Users.filter(u => u.Role == RoleName.ADMIN &&  u._id != user._id)
             this.dataStore.Admin.PlayerUsers = this.dataStore.Admin.Users.filter(u => u.Role == RoleName.PLAYER &&  u._id != user._id)
             this.dataStore.Admin.FacilitatorUsers = this.dataStore.Admin.Users.filter(u => u.Role == RoleName.FACILITATOR &&  u._id != user._id)
-            console.log(AdminCtrl.GetInstance().dataStore.Admin.Users, this.dataStore.Admin.Users)
+            console.log(AdminCtrl.GetInstance().dataStore.Admin.Users, this.dataStore.Admin.Users, this.dataStore.Admin.AdminUsers)
             this.dataStore.Admin.DeletionUser = null; 
         })
     }
