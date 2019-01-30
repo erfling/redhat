81997919b1892d7d0b7bac823f65a2cdf33657a4
Matt Breeden <breeden@gnuidea.net>
2019-01-30 15:26:35 -0500

    typo fix

--- client/game/Game.tsx
+++ client/game/Game.tsx
@@ -541,7 +541,7 @@ export default class Game extends BaseComponent<any, IControllerDataStore & { Ga
                             </div>
                             }
                             <pre>{JSON.stringify(this.state.ApplicationState.UnreadMessages, null, 2)}</pre>
-                            <pre>{JSON.stringify(this.state.ApplicationState.CurrentMessagess, null, 2)}</pre>
+                            <pre>{JSON.stringify(this.state.ApplicationState.CurrentMessages, null, 2)}</pre>
                             <Switch>
                                 <Route path="/game/welcome" component={Welcome} />
                                 <Route path="/game/peopleround" component={PeopleRound} />
