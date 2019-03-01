2cb7170ab0a16f4b1c054f397ca8c84cec63f689
Matt Breeden <breeden@gnuidea.net>
2019-03-01 13:35:58 -0500

    temp change url for emails

--- api/controllers/EmailCtrl.ts
+++ api/controllers/EmailCtrl.ts
@@ -21,12 +21,12 @@ export default abstract class EmailCtrl {
             from: 'Source Stream Experience <matt@sapienexperience.com>', // sender address
             to: recipient.Email, // list of receivers
             subject: 'Sign Up', // Subject line
-            html: "<p>You've been invited to be a" + ((recipient.Role == RoleName.ADMIN) ? "n " : " ") + " " + recipient.Role.toLowerCase() + " for Source Stream Experience. Click the link below and enter a password to join.</p><a href='http://sourcestreamexperience.com/login/join?token=" + token + "'>Sign up</a>"// plain text body
+            html: "<p>You've been invited to be a" + ((recipient.Role == RoleName.ADMIN) ? "n " : " ") + " " + recipient.Role.toLowerCase() + " for Source Stream Experience. Click the link below and enter a password to join.</p><a href='http://planetsapientestsite.com/login/join?token=" + token + "'>Sign up</a>"// plain text body
         };
 
         if (isReset) {
             mailOptions.subject = "Sapien Experience Password Reset"
-            mailOptions.html = "<p>Click the link to reset your Source Stream Experience admin password.</p><a href='http://sourcestreamexperience.com/login/join?token=" + token + "'>Reset Password</a><p>If you didn't request a new password, please contact Sapien Experience administrators.</p>"// plain text body
+            mailOptions.html = "<p>Click the link to reset your Source Stream Experience admin password.</p><a href='http://planetsapientestsite.com/login/join?token=" + token + "'>Reset Password</a><p>If you didn't request a new password, please contact Sapien Experience administrators.</p>"// plain text body
         }
 
         transporter.sendMail(mailOptions, function (err, info) {
